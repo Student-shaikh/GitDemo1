0bf93505992e4fb90ad4a32353ad17b81157c6eb
mohsin <mohsinrazakshaikh@gmail.com>
2024-01-04 12:02:26 +0530

    this commit from second

--- src/Day1.java
+++ src/Day1.java
@@ -6,6 +6,13 @@ public class Day1 {
 	public void demo()
 	{
 		System.out.println("Hello");
+		System.out.println("Mohsin");
+	}
+	
+	@Test
+	public void demo2()
+	{
+		System.out.println("Mohsin");
 	}
 
 }
